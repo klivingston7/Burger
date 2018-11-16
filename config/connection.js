c6643fd0cd504c415afa1f5d0dfb71afbcf41f3a
KLIV <wclivingston5@gmail.com>
2018-11-15 19:57:00 -0600

    update connection

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,21 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
+const Mysql = require("mysql");
 
-var connection = require("../config/connection.js");
+// connection configuration
+let config;
 
-if(process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL); 
-}else {
-    connection = mysql.createConnection({
-      host: "mwgmw3rs78pvwk4e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-      port: 3306,
-      user: "yvyunsk8c9dms283",
-      password: "ao33frsz8lj8axm6",
-      database: "xot5hui6md7xtqid"
-  });
-};
+// db connection config using JawsDB
+if (process.env.JAWSDB_URL) {
+	config = process.env.JAWSDB_URL;
+}
+else {
+	// default db config for local db
+	config = {
+		host: "localhost",
+		user: "root",
+		password: "121025",
+		database: "burgers_db"
+	};
+}
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-// Export connection for our ORM to use.
-module.exports = connection;
+// export connection to burgers_db
+module.exports = Mysql.createConnection(config);
